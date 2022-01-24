0666c5190a116c623bf2efccaf5430843f591bd8
katiepalumbo <palumbo.katie@gmail.com>
2022-01-24 23:48:46 +0100

    setup

---
+++ main.js
@@ -0,0 +1,6 @@
+let fullName = document.getElementById(full-name);
+let age = document.getElementById(age);
+
+let genera = document.getElementById(genera);
+let annulla = document.getElementById(annulla);
+
