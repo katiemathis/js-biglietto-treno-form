51fade733b84fc2cc4e8d17a6d421bfed45b32a9
katiepalumbo <palumbo.katie@gmail.com>
2022-02-03 23:20:33 +0100

    ho pulito il codice js

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
 //definire variabili
-
-    let nome = document.getElementById('nome');
-    //nome.innerHTML = (nome.value);
-
-
-    let km = document.getElementById('km');
-    //km.innerHTML = (km.value);
+let nome = document.getElementById('nome');
+let km = document.getElementById('km');
 
 
+    
+//in order to let dropdown menu work...
 
-
-
-    //for dropdown menu
-
-    Array.from(document.querySelector("#age").options).forEach(function(/*option_element*/) {
+Array.from(document.querySelector("#age").options).forEach(function(/*option_element*/) {
         /*let option_text = option_element.text;
         let option_value = option_element.value;
         let is_option_selected = option_element.selected;
@@ -23,49 +16,44 @@
         console.log('Option Selected : ' + (is_option_selected === true ? 'Yes' : 'No'));
 
         console.log("\n\r");*/
-    });
+});
 
-    //calcola prezzo biglietto
+//calcola prezzo biglietto
 
-    let genera = document.getElementById('genera');
-    genera.addEventListener('click', function () {
-        let prezzo = ((km.value) * .21).toFixed(2);
+let genera = document.getElementById('genera');
+genera.addEventListener('click', function () {
+    let prezzo = ((km.value) * .21).toFixed(2);
 
-        if (age.value == 'minorenni') {
-            console.log('Your name is: ' + nome.value);
-            console.log('You want to travel ' + km.value + ' km');
-            console.log('Your age is: ' + age.value);
-            console.log('Your biglietto costs:  $' + (prezzo * 0.81).toFixed(2));
-        } else if (age.value == 'senior') {
-            console.log('Your name is: ' + nome.value);
-            console.log('You want to travel ' + km.value + ' km');
-            console.log('Your age is: ' + age.value);
-            console.log('Your biglietto costs:  $' + (prezzo * 0.6).toFixed(2));
-        } else {
-            console.log('Your name is: ' + nome.value);
-            console.log('You want to travel ' + km.value + ' km');
-            console.log('Your age is: ' + age.value);
-            console.log('Your biglietto costs:  $' + prezzo);
-        }
-        
-
-
-
-
-    })
-
-    let annulla = document.getElementById('annulla');
-    annulla.addEventListener('click', function () {
-        nome.value = '';
-        km.value = 0;
-        age.value = '';
+    if (age.value == 'minorenni') {
+        console.log('Your name is: ' + nome.value);
+        console.log('You want to travel ' + km.value + ' km');
+        console.log('Your age is: ' + age.value);
+        console.log('Your biglietto costs:  $' + (prezzo * 0.81).toFixed(2));
+    } else if (age.value == 'senior') {
+        console.log('Your name is: ' + nome.value);
+        console.log('You want to travel ' + km.value + ' km');
+        console.log('Your age is: ' + age.value);
+        console.log('Your biglietto costs:  $' + (prezzo * 0.6).toFixed(2));
+    } else {
         console.log('Your name is: ' + nome.value);
         console.log('You want to travel ' + km.value + ' km');
         console.log('Your age is: ' + age.value);
         console.log('Your biglietto costs:  $' + prezzo);
+    }
+});
+
+let annulla = document.getElementById('annulla');
+annulla.addEventListener('click', function () {
+    nome.value = '';
+    km.value = 0;
+    age.value = '';
+    console.log('Your name is: ' + nome.value);
+    console.log('You want to travel ' + km.value + ' km');
+    console.log('Your age is: ' + age.value);
+    console.log('Your biglietto costs:  $' + prezzo);
 
 
-    })
+})
 
 
 
