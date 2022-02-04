7240d2e085e03be7e56bd3b32ccd0267a79dc6c9
katiepalumbo <palumbo.katie@gmail.com>
2022-02-04 11:11:50 +0100

    inizio a stampare in HTML

--- main.js
+++ main.js
@@ -18,23 +18,61 @@ Array.from(document.querySelector("#age").options).forEach(function(/*option_ele
         console.log("\n\r");*/
 });
 
+//generate random number for carozza
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+    
+};
+
 //calcola prezzo biglietto
 
 let genera = document.getElementById('genera');
 genera.addEventListener('click', function () {
+
+
+
+
+    //remove "hidden" class from bottom container
+    document.querySelector('#bottom').classList.remove('hidden');
+    document.querySelector('#biglietto-title').classList.remove('hidden');
+
+    //define variables for function
     let prezzo = ((km.value) * .21).toFixed(2);
+    let nomeBiglietto = document.getElementById('tuo-nome');
+    let etaBiglietto = document.getElementById('tua-eta');
+    let kmBiglietto = document.getElementById('tuo-viaggio');
+    let prezzoBiglietto = document.getElementById('costo-biglietto');
+    let carozzaBiglietto = document.getElementById('tua-carozza');
+
+    //stampa il numero carozza
+    carozzaBiglietto.innerHTML = 'You are in carozza number: ' + getRandomInt(1,9);
+
+
+
+    //conditional to calculate the correct price based on age
 
     if (age.value == 'minorenni') {
+        nomeBiglietto.innerHTML = 'Your name is: ' + nome.value;
+        etaBiglietto.innerHTML = 'You qualify for the sconto ' + age.value;
+        kmBiglietto.innerHTML = 'You want to travel ' + km.value + ' km';
+        prezzoBiglietto.innerHTML = 'Your biglietto costs:  $' + (prezzo * 0.8).toFixed(2);
         console.log('Your name is: ' + nome.value);
         console.log('You want to travel ' + km.value + ' km');
         console.log('Your age is: ' + age.value);
-        console.log('Your biglietto costs:  $' + (prezzo * 0.81).toFixed(2));
+        console.log('Your biglietto costs:  $' + (prezzo * 0.8).toFixed(2));
     } else if (age.value == 'senior') {
+        nomeBiglietto.innerHTML = 'Your name is: ' + nome.value;
+        etaBiglietto.innerHTML = 'You qualify for the sconto ' + age.value;
+        kmBiglietto.innerHTML = 'You want to travel ' + km.value + ' km';
+        prezzoBiglietto.innerHTML = 'Your biglietto costs:  $' + (prezzo * 0.6).toFixed(2);
         console.log('Your name is: ' + nome.value);
         console.log('You want to travel ' + km.value + ' km');
         console.log('Your age is: ' + age.value);
         console.log('Your biglietto costs:  $' + (prezzo * 0.6).toFixed(2));
     } else {
+        nomeBiglietto.innerHTML = 'Your name is: ' + nome.value;
+        kmBiglietto.innerHTML = 'You want to travel ' + km.value + ' km';
+        prezzoBiglietto.innerHTML = 'Your biglietto costs:  $' + prezzo;
         console.log('Your name is: ' + nome.value);
         console.log('You want to travel ' + km.value + ' km');
         console.log('Your age is: ' + age.value);
@@ -42,15 +80,15 @@ genera.addEventListener('click', function () {
     }
 });
 
+//reset form and add "hidden" class to bottom container
 let annulla = document.getElementById('annulla');
 annulla.addEventListener('click', function () {
+    document.querySelector('#bottom').classList.add('hidden');
+    document.querySelector('#biglietto-title').classList.add('hidden');
     nome.value = '';
     km.value = 0;
     age.value = '';
-    console.log('Your name is: ' + nome.value);
-    console.log('You want to travel ' + km.value + ' km');
-    console.log('Your age is: ' + age.value);
-    console.log('Your biglietto costs:  $' + prezzo);
+
 
 
 })
